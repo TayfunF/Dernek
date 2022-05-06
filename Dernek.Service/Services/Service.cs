739e2ef0b143db8ebe652bb4bccd51b706b2807a
TayfunF <firtinatayfun08@gmail.com>
2022-05-06 16:56:40 +0300

    Service Created

---
+++ Dernek.Service/Services/Service.cs
@@ -0,0 +1,79 @@
+﻿using Dernek.Core.Repositories;
+using Dernek.Core.Services;
+using Dernek.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dernek.Service.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Service(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<T> TAddAsync(T entity)
+        {
+            await _repository.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> TAddRangeAsync(IEnumerable<T> entities)
+        {
+            await _repository.AddRangeAsync(entities);
+            await _unitOfWork.CommitAsync();
+
+            return entities;
+        }
+
+        public async Task<bool> TAnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _repository.AnyAsync(expression);
+        }
+
+        public async Task<IEnumerable<T>> TGetAllAsync()
+        {
+            return await _repository.GetAll().ToListAsync();
+        }
+
+        public async Task<T> TGetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task TRemoveAsync(T entity)
+        {
+            _repository.Remove(entity);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task TRemoveRangeAsync(IEnumerable<T> entities)
+        {
+            _repository.RemoveRange(entities);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task TUpdateAsync(T entity)
+        {
+            _repository.Update(entity);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public IQueryable<T> TWhere(Expression<Func<T, bool>> expression)
+        {
+            return _repository.Where(expression);
+        }
+    }
+}
