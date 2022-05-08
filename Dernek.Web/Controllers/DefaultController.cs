4bcfd7caa0cd59b4ea72a7578ce5f81de2f80936
TayfunF <firtinatayfun08@gmail.com>
2022-05-08 21:05:42 +0300

    Carousel Created

--- Dernek.Web/Controllers/DefaultController.cs
+++ Dernek.Web/Controllers/DefaultController.cs
@@ -7,17 +7,19 @@ namespace Dernek.Web.Controllers
     public class DefaultController : Controller
     {
         private readonly IBlogService _blogService;
+        private readonly ICarouselService _carouselService;
 
-        public DefaultController(IBlogService blogService)
+        public DefaultController(IBlogService blogService, ICarouselService carouselService)
         {
             _blogService = blogService;
+            _carouselService = carouselService;
         }
 
         public async Task<IActionResult> Index()
         {
             var GetBlogList = await _blogService.TGetAllAsync();
-
             ViewBag.GetLastBlog = await _blogService.TGetLastBlogAsync();
+            ViewBag.GetCarouselList = await _carouselService.TGetAllAsync();
 
             //ViewBag.Get3Blogs = await _blogService.TGetLast3BlogsAsync();
 
